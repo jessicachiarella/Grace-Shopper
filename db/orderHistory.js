b8b3eafeb4b95a9faa7bf4b537b0bbe97bdf1891
Prisana Cheng <chengprisana@gmail.com>
2022-08-10 16:52:47 -0500

    successfully added seed data

--- db/orderHistory.js
+++ db/orderHistory.js
@@ -1,12 +1,12 @@
 const client = require("./client");
 
 
-async function createOrderHistory({ cartId, datePurchased }) { 
+async function createOrderHistory({ cartId }) { 
     const {rows:[orderHistory]}  = await client.query(`
-      INSERT INTO orderHistory("cartId", "datePurchased")
-      VALUES ($1, $2)
+      INSERT INTO "orderHistory"("cartId")
+      VALUES ($1)
       RETURNING *;
-    `, [cartId, datePurchased]);
+    `, [cartId]);
     
     return orderHistory;
   }
