f77b405d02a8107be12a3a773847e67e1bc942a1
jessicachiarella <jessica.chiarella@gmail.com>
2022-08-10 14:15:47 -0500

    created order history and order items dummy data and create functions

---
+++ db/orderItems.js
@@ -0,0 +1,17 @@
+const client = require("./client");
+
+//need to address price
+async function createOrderItem({ cartId, productId, quantity=1, price }) { 
+    const {rows:[orderItem]}  = await client.query(`
+      INSERT INTO orderItems("cartId", "productId", quantity, price)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [cartId, productId, quantity, price]);
+    
+    return orderItem;
+  }
+
+
+module.exports = {
+    createOrderItem
+}
