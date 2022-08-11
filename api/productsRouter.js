c2fe805329f222b736d4296d33595866721aabd8
Edward <duongedward44@gmail.com>
2022-08-11 14:44:40 -0500

    worked on usersRouter and productsRouter

---
+++ api/productsRouter.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { getAllProducts, getProductsByCategoryId, getProductById } = require('../db');
+
+
+//GET all products
+
+productsRouter.get("/", async (req, res, next) => {
+    try {
+        const products = await getAllProducts()
+        res.send(products)
+    } catch (error) {
+        next(error);
+    }
+})
+
+//GET product by id
+
+productRouter.get("/productId", async (req, res, next) => {
+    try {
+        if (! await getProductById(req.params.productId)) {
+            res.send({ error: "product error", message: `Product ${req.params.productId} not found`, name: "product error" })
+        }
+        const {productId } = req.params
+        const products = await getProductsByCategoryId({ id: productId })
+        res.send(products)``
+    } catch (error) {
+        next(error);
+    }
+})
+
+module.exports = productRouter;
