e33eb590875f0dd451c8449bf7272d7810cebd4a
jessicachiarella <jessica.chiarella@gmail.com>
2022-08-17 11:26:52 -0500

    working on add to cart

--- src/components/SingleProducts.js
+++ src/components/SingleProducts.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getProductById } from "../api/index";
 import { useParams } from "react-router-dom";
 
-const SingleProducts = () => {
+const SingleProducts = ({ cart, setCart, isLoggedIn }) => {
   const { id } = useParams();
   // console.log(params, "this is our params")
   console.log(id, "this is our id");
@@ -13,23 +13,33 @@ const SingleProducts = () => {
       setSingleProduct(results);
     });
   }, []);
+
   console.log(singleProduct, "this is singleproduct");
   console.log(singleProduct.products, "this is the .name");
   return (
     <div id="SingleProductPage">
       <div>
-        
         {singleProduct && singleProduct.products ? (
           <div id="Product" key={singleProduct.products.id}>
             <h2 id="name">{singleProduct.products.name}</h2>
             <p id="description">{singleProduct.products.description}</p>
             <p id="price">${singleProduct.products.price}</p>
-            {singleProduct.products.inStock ? <p id="inStock">In stock</p> :  <p id="inStock">Out of stock</p> } 
+            {singleProduct.products.inStock ? (
+              <p id="inStock">In stock</p>
+            ) : (
+              <p id="inStock">Out of stock</p>
+            )}
             <img src={singleProduct.products.image_url} width={400} />
-            <button id="checkoutButton">Add to Cart</button>
+            <AddToCart
+              cart={cart}
+              setCart={setCart}
+              productId={singleProduct.products.id}
+              productName={singleProduct.products.name}
+              productPrice={singleProduct.products.price}
+              isLoggedIn={isLoggedIn}
+            />
           </div>
         ) : null}
-
       </div>
     </div>
   );
