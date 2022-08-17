070f7cae682baabc470aaa2e9f70ba5936bb640a
Edward <duongedward44@gmail.com>
2022-08-17 14:10:33 -0500

    working on place order button

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,15 +3,18 @@ import { NavLink } from "react-router-dom";
 import Register from "./users/Register";
 
 
-function Checkout({ setIsLoggedIn, setEmail }) {
+const Checkout = ({ setIsLoggedIn, setEmail }) => {
+async function onClick() {
+getCart
+}
   return (
     <>
       <h1>Check Out</h1>
       <Register setIsLoggedIn={setIsLoggedIn} setEmail={setEmail} />
       <h1>Order Summary</h1>
 
-      {/* <button onClick="window.location.href='http://localhost:3000/Congratulations';">Place Order</button> */}
-      <button><NavLink className="CategoryLink" to="/Congratulations">Place Order</NavLink></button>
+      <button onClick="window.location.href='http://localhost:3000/Congratulations';">Place Order</button>
+      {/* <button><NavLink className="CategoryLink" to="/Congratulations">Place Order</NavLink></button> */}
 
     </>
   );
