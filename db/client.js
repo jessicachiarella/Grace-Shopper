3ea7cd9c39c7d124efdb67cb3a08400a1f182f49
Carlos Nava <chnava@me.com>
2022-08-17 09:30:02 -0500

    heroku test

--- db/client.js
+++ db/client.js
@@ -1,26 +1,39 @@
-// Connect to DB
+// // Connect to DB
+// const { Client } = require('pg');
+
+// // change the DB_NAME string to whatever your group decides on
+// const DB_NAME = 'Full-Bloom';
+
+// const DB_URL =
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+
+// let client;
+
+// // github actions client config
+// if (process.env.CI) {
+//   client = new Client({
+//     host: 'localhost',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'postgres',
+//     database: 'postgres',
+//   });
+// } else {
+//   // local / heroku client config
+//   client = new Client(DB_URL);
+// }
+
+// module.exports = {client};
+
+
+
 const { Client } = require('pg');
 
-// change the DB_NAME string to whatever your group decides on
-const DB_NAME = 'Full-Bloom';
+const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/Full-Bloom';
 
-const DB_URL =
-  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
-
-let client;
-
-// github actions client config
-if (process.env.CI) {
-  client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-  });
-} else {
-  // local / heroku client config
-  client = new Client(DB_URL);
-}
+const client = new Client({
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 module.exports = {client};
