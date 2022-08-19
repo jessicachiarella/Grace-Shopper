46b917ed4806ce4ff246210c65090fbb1ba7f2dc
Prisana Cheng <chengprisana@gmail.com>
2022-08-19 12:44:47 -0500

    deleted console logs

--- src/components/DeleteFromCart.js
+++ src/components/DeleteFromCart.js
@@ -2,11 +2,9 @@ import React from "react";
 import { deleteProduct, getMyInfo, getUnpurchasedCart, getOrderItemByCart } from "../api";
 
 const DeleteFromCart= ({ cart, setCart}) => {
-    // console.log(cart, "productId from delete")
   async function handleDelete(event) {
     event.preventDefault();
     const orderItem = await getOrderItemByCart(cart.cartId)
-    // console.log(orderItem, "this is orderITem")
     await deleteProduct(orderItem.id);
     const user = await getMyInfo(localStorage.getItem("token"))
     const result = await getUnpurchasedCart(user.id);
