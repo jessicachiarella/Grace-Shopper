f407262ff8457c6c90fcee93ac0e769c9bb4504a
jessicachiarella <jessica.chiarella@gmail.com>
2022-08-23 12:58:46 -0500

    home screen done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,11 @@ import { NavLink } from "react-router-dom";
 
 function Home() {
     return (
-        <main className="homeScreen">
-            <a href="/landing.jpeg" >
-            <div className="homeShopAllPlants">
-            <NavLink className="ShopAllPlants" to="/RenderAllPlants">
+        <div className="homeScreen">
+            <NavLink  className="ShopAllPlants" to="/RenderAllPlants">
               SHOP ALL PLANTS
             </NavLink>
-            </div>
-            </a>
-        </main>
+        </div>
     )
 }
 
